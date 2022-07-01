87b61f17ab0b56ac6b2b59ae0cdb2d00e76f6621
Quade Houston <quadehouston@gmail.com>
2022-07-01 16:32:12 -0400

    commit tree e6dd39baa75c9cba2568d87795ae105bbd5f716d

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(a, b) {
+    return a + b
+}
+function subtract(a, b) {
+    return a - b
+}
+function multiply(a, b) {
+    return a*b
+}
+function divide(a, b) {
+    return a / b
+}
+n = 5
+
+function increment(n) {
+    n += 1
+    return n
+}
+function decrement(n) {
+    n -= 1
+    return n
+}
+function makeInt (n, l) {
+    l = parseInt (n, 10)
+    return l
+}
+function preserveDecimal(n, l) {
+    l = parseFloat(n)
+    return l
+}
